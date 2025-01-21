d98c2a0720b06f8969af760c0f0b311db58e2cee
iraavas <iravas981@icloud.com>
2025-01-21 16:12:51 +0300

    added a file with a list of words, parsed the list

--- src/app/main.py
+++ src/app/main.py
@@ -1,21 +1,19 @@
 from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("api/words")
-def get_words():
+@app.get("/")
+async def hello():
+        return "123"
+
+@app.get("/api/words")
+async def get_words():
     with open('words.txt', 'r', encoding='utf-8') as file:
         data = file.read()
         return {
             "words": data.split()
         }
-
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
-
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
